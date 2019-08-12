b939b8f331154d3f83de421b67a4b28b32a58e08
rohitPrajapati1897 <rohitpjpti18@gmail.com>
2019-08-12 09:57:51 +0530

    saved models

--- CourierProject/users/models.py
+++ CourierProject/users/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
