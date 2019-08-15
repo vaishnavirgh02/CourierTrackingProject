009bc7ecc1847ab74a87e36b8dd246c73555eaff
rohitPrajapati1897 <rohitpjpti18@gmail.com>
2019-08-15 23:52:41 +0530

    Minor changes in logout page and consignment models (changed return value of User's __str__ method)

--- CourierProject/users/models.py
+++ CourierProject/users/models.py
@@ -3,23 +3,30 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+def new_str_method(self):
+        return self.first_name + ' ' + self.last_name
 
-    def __str__(self):
-        return f'{self.user.first_name} Profile' 
+User.add_to_class("__str__", new_str_method)
+
+        
+
+class Profile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        bio = models.TextField(max_length=500, blank=True)
+        location = models.CharField(max_length=30, blank=True)
+        birth_date = models.DateField(null=True, blank=True)
+
+        def __str__(self):
+                return f'{self.user.first_name} Profile' 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+        if created:
+                Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+        instance.profile.save()
 
 
 
